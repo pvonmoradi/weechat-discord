522d75ae1bd1cf9af5e69a37cb66d7297ac8fa96
Noskcaj <kcajdev@gmail.com>
2021-04-16 12:21:04 -0400

    Bring changes from mk3 branch, improved filtering and username lookup

--- find_token.py
+++ find_token.py
@@ -2,6 +2,10 @@ import subprocess
 import sys
 import string
 import platform
+import base64
+import urllib.request
+import json
+from functools import cache
 
 
 def strings(filename, min=4):
@@ -18,6 +22,27 @@ def strings(filename, min=4):
             yield result
 
 
+@cache
+def id2username(id):
+    try:
+        resp = urllib.request.urlopen(
+            "https://terminal-discord.vercel.app/api/lookup-user?id={}".format(id)
+        )
+        data = json.load(resp)
+        return data.get("username") or "Unknown"
+    except:
+        return "Unkown"
+
+
+def token2userid(token):
+    id_part = token.split(".")[0]
+    return base64.b64decode(id_part).decode()
+
+
+def token2username(token):
+    return id2username(token2userid(token))
+
+
 def run_command(cmd):
     output = subprocess.Popen(
         [cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
@@ -26,32 +51,39 @@ def run_command(cmd):
 
 
 def main():
+    skip_username_lookup = "--no-lookup" in sys.argv
     print("Searching for Discord localstorage databases...")
+    # First, we search for .ldb files, these are the leveldb files used by chromium to store localstorage data,
+    # which contains the discord token.
     rg = False
-    if platform.system() == "Darwin":
-        results = run_command("mdfind \"kMDItemDisplayName=='*.ldb'\"")
-    else:
-        try:
-            subprocess.check_output(["rg", "--version"])
-            results = run_command("rg ~/ --files -g '*.ldb'")
-            rg = True
-        except FileNotFoundError:
-            results = run_command("find ~/ -name '*.ldb'")
+    # Try and use ripgrep, because it's much faster, otherwise, fallback to `find`.
+    try:
+        subprocess.check_output(["rg", "--version"])
+        results = run_command("rg ~/ --files -g '*.ldb'")
+        rg = True
+    except FileNotFoundError:
+        results = run_command("find ~/ -name '*.ldb'")
 
     if len(results) == 0 and rg:
-        # try again, but search hidden directories
+        # Try again, but search hidden directories.
         results = run_command("rg ~/ --hidden --files -g '*.ldb'")
 
     if len(results) == 0:
         print("No databases found.")
         sys.exit(1)
 
-    discord_databases = list(filter(lambda x: "discord" in x, results))
+    # Only search for tokens in ldb files likely belonging to a discord applications local storage
+    # (this prevents searching browsers, but browser localstorage returns lots of false positives).
+    discord_databases = list(
+        filter(lambda x: "discord" in x and "Local Storage" in x, results)
+    )
 
+    # Then collect strings that look like discord tokens.
     token_candidates = set()
     for database in discord_databases:
         for candidates in map(lambda s: s.split(), strings(database, 40)):
             for candidate in candidates:
+                candidate = candidate[1:-1]
                 if len(candidate) < 15:
                     continue
                 if " " in candidate:
@@ -61,7 +93,11 @@ def main():
                     continue
                 if len(parts[1]) < 6:
                     continue
-                token_candidates.add(candidate[1:-1])
+                try:
+                    base64.b64decode(parts[0], validate=True)
+                except:
+                    continue
+                token_candidates.add(candidate)
 
     if len(token_candidates) == 0:
         print("No Discord tokens found")
@@ -69,7 +105,10 @@ def main():
 
     print("Possible Discord tokens found:\n")
     for token in token_candidates:
-        print(token)
+        if skip_username_lookup:
+            print("{}".format(token))
+        else:
+            print("@{}: {}".format(token2username(token), token))
 
 
 if __name__ == "__main__":
